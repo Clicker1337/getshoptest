108f86fb77ca7176982e7d6ffaf1af7a6d8eebf9
Clicker1337 <lolololo.lol@list.ru>
2023-10-22 00:27:42 +0700

    project initialization

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import s from './App.module.scss'
+
+function App() {
+
+    return (
+        <>
+            <div className={s.container}>
+                MainScreen
+            </div>
+        </>
+    )
+}
+
+export default App
